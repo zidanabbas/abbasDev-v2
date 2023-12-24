c446d4ed346112566f2d22607e3b33f390f4082b
zidanabbas <zidan.abbas28@gmail.com>
2023-12-24 23:36:29 +0700

    konfigurasi config tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      dark: "#121212",
+    },
+    animation: {
+      beat: "beat 0.7s ease-in-out infinite",
+      shake: "shake 2s linear infinite",
+    },
+    keyframes: {
+      beat: {
+        "0%": { transform: "scale(1.2)" },
+        "50%": { transform: "scale(1)" },
+        "100%": { transform: "scale(1.2)" },
+      },
+      shake: {
+        "0%": { transform: "rotate(0.0deg)" },
+        "10%": { transform: "rotate(14deg)" },
+        "20%": { transform: "rotate(-8deg)" },
+        "30%": { transform: "rotate(14deg)" },
+        "40%": { transform: "rotate(-4deg)" },
+        "50%": { transform: "rotate(10.0deg)" },
+        "60%": { transform: "rotate(0.0deg)" },
+        "100%": { transform: "rotate(0.0deg)" },
       },
     },
   },
+  darkMode: "class",
   plugins: [],
-}
-export default config
+};
+export default config;
