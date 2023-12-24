e5942ea281de38fe1c03d7463c0382e702d521aa
zidanabbas <zidan.abbas28@gmail.com>
2023-12-25 02:04:26 +0700

    menambahkan module skillist

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,6 +3,7 @@ import HeaderHome from "../components/header/Header";
 import { Divider } from "@nextui-org/react";
 import Career from "@/app/home/components/Career";
 import ContactCard from "@/app/home/components/ContactCard";
+import SkillLists from "@/app/home/components/SkillList";
 export default function HomePage() {
   return (
     <ContainerAOS>
