2f381a98960ed93209a650b2bc3e28ff740df795
zidanabbas <zidan.abbas28@gmail.com>
2023-12-25 02:07:52 +0700

    membuat skill card

---
+++ src/app/home/components/SkillCard.js
@@ -0,0 +1,13 @@
+import { Tooltip } from "@nextui-org/react";
+
+export default function SkillCard({ name, icon }) {
+  return (
+    <>
+      <Tooltip content={name} radius="sm" showArrow="true">
+        <div className="p-2 flex items-center justify-center">
+          <div className="w-6 h-6 ">{icon}</div>
+        </div>
+      </Tooltip>
+    </>
+  );
+}
