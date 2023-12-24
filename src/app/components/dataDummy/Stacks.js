79965f6369fa77a22ad82b3ee1d0d369a11d490a
zidanabbas <zidan.abbas28@gmail.com>
2023-12-25 01:57:10 +0700

    membuat folder data dummy

---
+++ src/app/components/dataDummy/Stacks.js
@@ -0,0 +1,127 @@
+import { BiLogoJavascript } from "react-icons/bi";
+import { BsFillBootstrapFill } from "react-icons/bs";
+import {
+  SiChakraui,
+  SiCss3,
+  SiExpo,
+  SiExpress,
+  SiFirebase,
+  SiFramer,
+  SiGithub,
+  SiGraphql,
+  SiGulp,
+  SiHtml5,
+  SiJavascript,
+  SiJest,
+  SiMui,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiNuxtdotjs,
+  SiPhp,
+  SiPrisma,
+  SiReact,
+  SiReacthookform,
+  SiReactquery,
+  SiReactrouter,
+  SiReacttable,
+  SiRedux,
+  SiSass,
+  SiStorybook,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+  SiVuedotjs,
+} from "react-icons/si";
+
+const iconSize = "100%";
+
+export const Stacks = [
+  {
+    name: "JavaScript",
+    icon: <SiJavascript size={iconSize} className="text-yellow-400" />,
+  },
+  {
+    name: "Next.js",
+    icon: <SiNextdotjs size={iconSize} />,
+  },
+  {
+    name: "React.js",
+    icon: <SiReact size={iconSize} className="text-sky-500" />,
+  },
+  {
+    name: "TailwindCSS",
+    icon: <SiTailwindcss size={iconSize} className="text-cyan-300" />,
+  },
+  {
+    name: "Bootstrap",
+    icon: <BsFillBootstrapFill size={iconSize} className="text-purple-500" />,
+  },
+  {
+    name: "Material UI",
+    icon: <SiMui size={iconSize} className="text-sky-400" />,
+  },
+  // {
+  //   name: 'Vite',
+  //   icon: <SiVite size={iconSize} className="text-purple-500" />,
+  // },
+  {
+    name: "ChakraUI",
+    icon: <SiChakraui size={iconSize} className="text-teal-500" />,
+  },
+  // {
+  //   name: 'React Native',
+  //   icon: <SiReact size={iconSize} className="text-sky-600" />,
+  // },
+  // {
+  //   name: 'Firebase',
+  //   icon: <SiFirebase size={iconSize} className="text-yellow-500" />,
+  // },
+  {
+    name: "Framer Motion",
+    icon: <SiFramer size={iconSize} />,
+  },
+  // {
+  //   name: 'Vue.js',
+  //   icon: <SiVuedotjs size={iconSize} className="text-green-500" />,
+  // },
+  // {
+  //   name: 'Express.js',
+  //   icon: <SiExpress size={iconSize} />,
+  // },
+  // {
+  //   name: 'Redux',
+  //   icon: <SiRedux size={iconSize} className="text-purple-500" />,
+  // },
+  // {
+  //   name: 'React Query',
+  //   icon: <SiReactquery size={iconSize} className="text-red-600" />,
+  // },
+  {
+    name: "HTML",
+    icon: <SiHtml5 size={iconSize} className="text-orange-500" />,
+  },
+  {
+    name: "CSS",
+    icon: <SiCss3 size={iconSize} className="text-blue-500" />,
+  },
+  // {
+  //   name: 'Node JS',
+  //   icon: <SiNodedotjs size={iconSize} className="text-green-600" />,
+  // },
+  {
+    name: "Github",
+    icon: <SiGithub size={iconSize} />,
+  },
+  // {
+  //   name: 'React Router',
+  //   icon: <SiReactrouter size={iconSize} className="text-pink-500" />,
+  // },
+  // {
+  //   name: 'React Hook Form',
+  //   icon: <SiReacthookform size={iconSize} className="text-pink-500" />,
+  // },
+  // {
+  //   name: 'React Table',
+  //   icon: <SiReacttable size={iconSize} className="text-rose-600" />,
+  // },
+];
