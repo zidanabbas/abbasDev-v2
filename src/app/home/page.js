9c69d373c1c8467ee35b6a292789131ae348bcd7
zidanabbas <zidan.abbas28@gmail.com>
2023-12-25 00:43:03 +0700

    menambahkan home page

---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+import ContainerAOS from "@/app/components/elements/ContainerAOS.js";
+import HeaderHome from "../components/header/Header";
+export default function HomePage() {
+  return (
+    <ContainerAOS>
+      <div>
+        <HeaderHome />
+      </div>
+    </ContainerAOS>
+  );
+}
