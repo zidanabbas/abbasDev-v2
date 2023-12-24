3b2148eda8a498edd5fd12aca3a6a0ca7c23d206
zidanabbas <zidan.abbas28@gmail.com>
2023-12-25 02:04:01 +0700

    menambahkan module divider, career, skilllist

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,19 @@
 import ContainerAOS from "@/app/components/elements/ContainerAOS.js";
 import HeaderHome from "../components/header/Header";
+import { Divider } from "@nextui-org/react";
+import Career from "@/app/home/components/Career";
+import ContactCard from "@/app/home/components/ContactCard";
 export default function HomePage() {
   return (
     <ContainerAOS>
-      <div>
+      <div className="">
         <HeaderHome />
+        <Divider orientation="horizontal" className="my-6" />
+        <Career />
+        <Divider orientation="horizontal" className="my-6" />
+        <SkillLists />
+        <Divider orientation="horizontal" className="my-6" />
+        <ContactCard />
       </div>
     </ContainerAOS>
   );
