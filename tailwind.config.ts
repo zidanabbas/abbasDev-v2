9b42e1651fb4c3285ddb7387e831cca2d26af943
zidanabbas <zidan.abbas28@gmail.com>
2023-12-26 23:34:35 +0700

    konfigurasi tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,29 +15,29 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      dark: "#121212",
-    },
-    animation: {
-      beat: "beat 0.7s ease-in-out infinite",
-      shake: "shake 2s linear infinite",
-    },
-    keyframes: {
-      beat: {
-        "0%": { transform: "scale(1.2)" },
-        "50%": { transform: "scale(1)" },
-        "100%": { transform: "scale(1.2)" },
+      colors: {
+        dark: "#121212",
       },
-      shake: {
-        "0%": { transform: "rotate(0.0deg)" },
-        "10%": { transform: "rotate(14deg)" },
-        "20%": { transform: "rotate(-8deg)" },
-        "30%": { transform: "rotate(14deg)" },
-        "40%": { transform: "rotate(-4deg)" },
-        "50%": { transform: "rotate(10.0deg)" },
-        "60%": { transform: "rotate(0.0deg)" },
-        "100%": { transform: "rotate(0.0deg)" },
+      animation: {
+        beat: "beat 0.7s ease-in-out infinite",
+        shake: "shake 2s linear infinite",
+      },
+      keyframes: {
+        beat: {
+          "0%": { transform: "scale(1.2)" },
+          "50%": { transform: "scale(1)" },
+          "100%": { transform: "scale(1.2)" },
+        },
+        shake: {
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
+        },
       },
     },
   },
