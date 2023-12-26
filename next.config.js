c8405fee2858af172bd7bbc6294e6f8381507ae3
zidanabbas <zidan.abbas28@gmail.com>
2023-12-27 00:13:54 +0700

    konfigurasi next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "i.scdn.co"],
+  },
+  env: {
+    // Tambahkan URL produksi atau URL yang sesuai dengan kebutuhan kamu
+    NEXT_PUBLIC_BASE_URL:
+      process.env.NODE_ENV === "production"
+        ? "https://example.com"
+        : "http://localhost:3000",
+  },
+  // ... konfigurasi lainnya
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
