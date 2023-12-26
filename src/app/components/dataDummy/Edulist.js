639b4641ab8014ccabc54e16ccefbef8457f0e34
zidanabbas <zidan.abbas28@gmail.com>
2023-12-27 01:43:46 +0700

    membuat data dummy education

---
+++ src/app/components/dataDummy/Edulist.js
@@ -0,0 +1,20 @@
+export const Edulist = [
+  {
+    title: "SMA PGRI 3 Jakarta Selatan",
+    logo: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1699433050/abbasImage/PGRI_3_JAKARTA_rtjyrn.png",
+    href: "https://www.smapgri3jkt.sch.id/",
+    year: "2015 - 2018",
+    school: "Senior High School",
+    gtw: "-",
+    jurusan: "Social Science",
+  },
+  {
+    title: "Pamulang University",
+    logo: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1699432867/abbasImage/Universitas-Pamulang_uajuvg.png",
+    href: "https://unpam.ac.id/",
+    year: "2018 - 2023",
+    school: "University",
+    gtw: "-",
+    jurusan: "Informatics Engineering",
+  },
+];
