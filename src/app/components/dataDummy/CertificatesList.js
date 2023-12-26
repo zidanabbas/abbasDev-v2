b2767a5150022bd3543aaa38eb0e1889ce048607
zidanabbas <zidan.abbas28@gmail.com>
2023-12-27 02:03:09 +0700

    membuat data dummy sertifikat

---
+++ src/app/components/dataDummy/CertificatesList.js
@@ -0,0 +1,26 @@
+export const CertificatesList = [
+  {
+    title: "Belajar Dasar Pemrograman Web",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616951/abbasImage/afi3yl0h5dl3g8lruz1n.png",
+    description:
+      "Learn the basic components of HTML and CSS which are the main foundations for becoming a front-end web developer.",
+    diberikan: "Sept 30, 2023",
+    berlaku: "Sept 30, 2026",
+    id_certificate: "N9ZO5Y44DPG5",
+    by: "dicoding",
+    slug: "belajar-dasar-pemrograman-web",
+  },
+  {
+    title: "Belajar Membuat Front-End Web untuk Pemula",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616953/abbasImage/bg9syhpa4oxiq8m886i0.png",
+    description:
+      "Study material regarding DOM manipulation, Events, and Web Storage to improve website functionality and interactivity.",
+    diberikan: "Oct 24, 2023",
+    berlaku: "Oct 24, 2026",
+    id_certificate: "EYX4YMN1JZDL",
+    by: "dicoding",
+    slug: "belajar-membuat-front-end-web-untuk-pemula",
+  },
+];
