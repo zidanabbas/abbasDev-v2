c18aa670032ea7c7affce2c533f22afdbd336747
zidanabbas <zidan.abbas28@gmail.com>
2023-12-27 02:30:38 +0700

    membuat slug about component sertifikat

---
+++ src/app/about/[slug]/page.js
@@ -0,0 +1,12 @@
+import ContainerAOS from "@/app/components/elements/ContainerAOS";
+import CertificateDetail from "@/app/about/components/CertificateDetail.js";
+
+export default function SlugCertificate() {
+  return (
+    <>
+      <ContainerAOS>
+        <CertificateDetail />
+      </ContainerAOS>
+    </>
+  );
+}
