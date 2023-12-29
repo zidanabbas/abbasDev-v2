fae28c0ae10b48b9e20ec6f29ae85d0843083c8b
zidanabbas <zidan.abbas28@gmail.com>
2023-12-30 00:19:50 +0700

    membuat slug project details

---
+++ src/app/projects/[slug]/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ContainerAOS from "@/app/components/elements/ContainerAOS";
+import ProjectDetails from "@/app/projects/components/ProjectDetails";
+
+const ProjectDetailPage = () => {
+  return (
+    <div>
+      <ContainerAOS>
+        <ProjectDetails />
+      </ContainerAOS>
+    </div>
+  );
+};
+
+export default ProjectDetailPage;
