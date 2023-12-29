54f21e6bc645b00f32a5e3f65ebf56632a537e17
zidanabbas <zidan.abbas28@gmail.com>
2023-12-29 22:47:48 +0700

    Menambahkan logo pada career

--- src/app/home/components/Career.js
+++ src/app/home/components/Career.js
@@ -30,14 +30,19 @@ export default function Career() {
                   key={index}
                   className="rounded-xl transition-all duration-300 shadow-sm lg:hover:shadow-md flex items-center gap-5 py-4 px-6 border border-neutral-300 dark:border-neutral-800 dark:bg-neutral-800"
                 >
-                  <div className="p-3 bg-neutral-200 dark:bg-neutral-600 rounded-full">
-                    {logo}
+                  <div className="bg-neutral-300 items-center dark:bg-neutral-600 rounded-full">
+                    <img
+                      src={logo}
+                      alt=""
+                      width={70}
+                      className="rounded-full"
+                    />
                   </div>
                   <div className="flex-col flex space-y-2">
-                    <h1>{profession}</h1>
+                    <h1> {profession}</h1>
                     <div className="flex items-center gap-1 md:gap-2 text-xs mt-1">
                       <Link
-                        href="/"
+                        href="https://www.instagram.com/iftaassociation/"
                         className="text-neutral-500 dark:text-neutral-400 underline underline-offset-2"
                       >
                         {name}
