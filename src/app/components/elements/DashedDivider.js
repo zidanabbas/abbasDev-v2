a29a20daac15cd9d2fee5078bc1aa6733b55e41c
zidanabbas <zidan.abbas28@gmail.com>
2023-12-29 23:11:17 +0700

    membuat element divider

---
+++ src/app/components/elements/DashedDivider.js
@@ -0,0 +1,11 @@
+function DashedDivider({ className }) {
+  return (
+    <>
+      <div
+        className={`border-b border-dashed border-neutral-600 dark:border-neutral-500 ${className}`}
+      ></div>
+    </>
+  );
+}
+
+export default DashedDivider;
