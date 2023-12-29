5f6b0f4eaa1a37ce4104254a95c737e86a593253
zidanabbas <zidan.abbas28@gmail.com>
2023-12-29 22:10:59 +0700

    memperbaiki margin bagian education

--- src/app/about/components/Education.js
+++ src/app/about/components/Education.js
@@ -6,8 +6,8 @@ import { TbSchool } from "react-icons/tb";
 export default function Education() {
   return (
     <>
-      <div className="space-y-6">
-        <div className="space-y-2 ">
+      <div className="space-y-6 mt-3">
+        <div className="space-y-2">
           <Heading title="Education" icon={<TbSchool className="mr-1" />} />
           <SubHeading>
             <p>My educational journey.</p>
