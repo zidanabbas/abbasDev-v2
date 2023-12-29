2b12f5460a7e88ac9791639eda4cfd94f8b6c2d9
zidanabbas <zidan.abbas28@gmail.com>
2023-12-29 23:27:30 +0700

    memperbaiki map key index

--- src/app/roadmap/components/CourseList.js
+++ src/app/roadmap/components/CourseList.js
@@ -42,6 +42,7 @@ function CourseList() {
           <div className="flex flex-col space-y-2 mt-6">
             {FRONTEND.map((item, index) => (
               <motion.div
+                key={index}
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: index * 0.05 }}
@@ -62,6 +63,7 @@ function CourseList() {
           <div className="flex flex-col space-y-2 mt-6">
             {MASTERING_REACT.map((item, index) => (
               <motion.div
+                key={index}
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: index * 0.05 }}
