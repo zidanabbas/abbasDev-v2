0f94361f96c993058b9f87c754790ca6c9a90257
zidanabbas <zidan.abbas28@gmail.com>
2023-12-29 21:22:42 +0700

    update data sertifikat

--- src/app/components/dataDummy/CertificatesList.js
+++ src/app/components/dataDummy/CertificatesList.js
@@ -1,5 +1,16 @@
 export const CertificatesList = [
   {
+    title: "Sertifikat Kompetensi BNSP",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703857652/abbasImage/ypmoasgzwzxjoitjhkpw.png",
+    description:
+      "Membuat aplikasi web dengan menggunakan Object-Oriented Programming yang di selenggarakan oleh Badan Sertifikat Nasional (BNSP) sebagai persyaratan untuk mendapatkan sertifikat kompetensi di universitas pamulang.",
+    diberikan: "3 tahun",
+    berlaku: "sampai 3 tahun",
+    by: "Badan Sertifikat Nasional (BNSP)",
+    slug: "sertifikat-kompetensi",
+  },
+  {
     title: "Belajar Dasar Pemrograman Web",
     image:
       "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616951/abbasImage/afi3yl0h5dl3g8lruz1n.png",
@@ -8,7 +19,7 @@ export const CertificatesList = [
     diberikan: "Sept 30, 2023",
     berlaku: "Sept 30, 2026",
     id_certificate: "N9ZO5Y44DPG5",
-    by: "dicoding",
+    by: "Dicoding",
     slug: "belajar-dasar-pemrograman-web",
   },
   {
@@ -20,7 +31,67 @@ export const CertificatesList = [
     diberikan: "Oct 24, 2023",
     berlaku: "Oct 24, 2026",
     id_certificate: "EYX4YMN1JZDL",
-    by: "dicoding",
+    by: "Dicoding",
     slug: "belajar-membuat-front-end-web-untuk-pemula",
+  },
+  {
+    title: "Belajar Membuat Aplikasi Web dengan React",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616952/abbasImage/fockdfivfxn7925rjxp5.png",
+    description:
+      "Learn about basic React concepts like composition, declarative code, unidirectional data flow, elements, reusable property components",
+    diberikan: "Oct 31, 2023",
+    berlaku: "Oct 31, 2026",
+    id_certificate: "6RPN4N8RQX2M",
+    by: "Dicoding",
+    slug: "belajar-membuat-aplikasi-web-dengan-react",
+  },
+  {
+    title: "Belajar Dasar Pemrograman JavaScript",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616951/abbasImage/n7khtvawpmxh1vyo20sk.png",
+    description:
+      "Learn the basics of JavaScript, Data Structures, Functions, Object-Oriented Programming, Functional Programming, Environment setup, Modules, Error Handling, Concurrency, Node Package Manager/NPM, and JavaScript testing",
+    diberikan: "Oct 9, 2023",
+    berlaku: "Oct 9, 2026",
+    id_certificate: "EYX4YK3GOZDL",
+    by: "Dicoding",
+    slug: "belajar-dasar-pemrograman-javascript",
+  },
+  {
+    title: "Memulai Dasar Pemrograman untuk Menjadi Pengembang Software",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616951/abbasImage/y1dbajhrxl2iv8fuvkm8.png",
+    description:
+      "Learn theory and methodology in understanding application needs from the user's side along with the application's technical specifications. Implementation of skills into case studies creates application requirements from the user side as well as from the technical specifications side. Learn work attitude tips when researching, analyzing, and evaluating application requirements.",
+    diberikan: "Oct 16, 2023",
+    berlaku: "Oct 16, 2026",
+    id_certificate: "EYX4YOL3RZDL",
+    by: "Dicoding",
+    slug: "memulai-dasar-pemrograman-untuk-menjadi-pengembang-software",
+  },
+  {
+    title: "Belajar Dasar Git dengan GitHub",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616951/abbasImage/wkr1jxmgickpwbwnkeyy.png",
+    description:
+      "Learn the concepts of how git and github work such as git Branch, git checkout, git push, git pull, git commit, and team collaboration using github",
+    diberikan: "Oct 9, 2023",
+    berlaku: "Oct 9, 2026",
+    id_certificate: "81P27OQEJZOY",
+    by: "Dicoding",
+    slug: "belajar-dasar-git-dengan-github",
+  },
+  {
+    title: "Belajar Dasar Structured Query Language (SQL)",
+    image:
+      "https://res.cloudinary.com/dlshk9mf6/image/upload/v1703616951/abbasImage/aqj4hxyhj1yjhwblxtld.png",
+    description:
+      "Introduce the data in general and identify its importance. learn types of data, types of data, then the differences between data, databases, and datasets, as well as getting to know relational databases, identifying DBMS that are commonly used to learn Structured Query Language (SQL), and learning queries such as insert, select * from, in where, to delete.",
+    diberikan: "Aug 29, 2023",
+    berlaku: "Aug 29, 2026",
+    id_certificate: "QLZ9QJEREZ5D",
+    by: "Dicoding",
+    slug: "belajar-dasar-structured-query-language-sql",
   },
 ];
