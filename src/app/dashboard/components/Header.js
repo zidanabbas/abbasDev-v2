6489aba6f8d9f43ffa3d0fc1b2da8a8c87e950f9
zidanabbas <zidan.abbas28@gmail.com>
2024-01-02 01:21:26 +0700

    membuat header dashboard

--- src/app/dashboard/components/Header.js
+++ src/app/dashboard/components/Header.js
@@ -1,2 +1,17 @@
 import Heading from "@/app/components/elements/Heading";
 import SubHeading from "@/app/components/elements/SubHeading";
+
+function Header({}) {
+  return (
+    <div className="space-y-6">
+      <div className="space-y-2">
+        <Heading title={"Dashboard"} />
+        <SubHeading>
+          <p>My personal Dashboard</p>
+        </SubHeading>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
