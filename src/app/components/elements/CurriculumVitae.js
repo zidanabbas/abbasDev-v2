1a7738a367e916fd87899b4f4ca1256d7e8a2879
zidanabbas <zidan.abbas28@gmail.com>
2024-01-03 15:26:50 +0700

    update tooltip pada CurriculumVitae.js

--- src/app/components/elements/CurriculumVitae.js
+++ src/app/components/elements/CurriculumVitae.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Link from "next/link";
 import { LuDownloadCloud } from "react-icons/lu";
+import { Tooltip } from "@nextui-org/react";
 
 export default function CurriculumVitae() {
   return (
     <>
-      <Link
-        href="/cv/CV_TranskipNilai_Ijazah_Zidane_Abbas.pdf"
-        target="_blank"
-        aria-label="curriculum-vitae"
-        download={"CV_TranskipNilai_Ijazah_Zidane_Abbas.pdf"}
-      >
-        <LuDownloadCloud size={24} className="text-3xl" />
-      </Link>
+      <Tooltip content="Download CV" radius="sm" showArrow="true">
+        <Link
+          href="/cv/CV_TranskipNilai_Ijazah_Zidane_Abbas.pdf"
+          target="_blank"
+          aria-label="curriculum-vitae"
+          download={"CV_TranskipNilai_Ijazah_Zidane_Abbas.pdf"}
+        >
+          <LuDownloadCloud size={24} className="text-3xl" />
+        </Link>
+      </Tooltip>
     </>
   );
 }
