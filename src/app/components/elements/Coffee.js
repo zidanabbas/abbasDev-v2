26143f07372bc58d1d2ae34c2db72ea2cd0e4e58
zidanabbas <zidan.abbas28@gmail.com>
2024-01-03 15:25:56 +0700

    update tooltip

--- src/app/components/elements/Coffee.js
+++ src/app/components/elements/Coffee.js
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import { BiCoffee } from "react-icons/bi";
 import React from "react";
+import { Tooltip } from "@nextui-org/react";
 
 export default function Coffee() {
   return (
     <>
-      <Link
-        href="https://saweria.co/zidanabbas"
-        target="_blank"
-        aria-label="saweria"
-      >
-        <BiCoffee size={24} className="text-3xl" />
-      </Link>
+      <Tooltip content="Saweria" radius="sm" showArrow="true">
+        <Link
+          href="https://saweria.co/zidanabbas"
+          target="_blank"
+          aria-label="saweria"
+        >
+          <BiCoffee size={24} className="text-3xl" />
+        </Link>
+      </Tooltip>
     </>
   );
 }
