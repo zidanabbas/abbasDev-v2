a43ae7e898d9a2ecdef4962e51ab4a9596e52495
zidanabbas <zidan.abbas28@gmail.com>
2024-01-03 10:56:59 +0700

    membuat data dummy untuk progress stack

---
+++ src/app/components/dataDummy/ProgressList.js
@@ -0,0 +1,38 @@
+export const ProgressLists = [
+  {
+    title: "Languages",
+    gradients: "from-yellow-500 via-orange-500 to-red-500",
+    lists: [
+      {
+        name: "JavaScript",
+        level: "60%",
+      },
+      {
+        name: "TypeScript",
+        level: "9%",
+      },
+      {
+        name: "Other",
+        level: "5%",
+      },
+    ],
+  },
+  {
+    title: "Frameworks",
+    gradients: "from-blue-500 via-indigo-500 to-purple-500",
+    lists: [
+      {
+        name: "Next.js",
+        level: "35%",
+      },
+      {
+        name: "React.js",
+        level: "28%",
+      },
+      {
+        name: "Other",
+        level: "10%",
+      },
+    ],
+  },
+];
