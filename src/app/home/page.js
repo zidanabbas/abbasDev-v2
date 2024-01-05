550f342f45bfaf723b0a9615422399ea3261f10e
zidanabbas <zidan.abbas28@gmail.com>
2024-01-05 22:47:28 +0700

    update home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,28 +4,21 @@ import { Divider } from "@nextui-org/react";
 import Career from "@/app/home/components/Career";
 import ContactCard from "@/app/home/components/ContactCard";
 import SkillLists from "@/app/home/components/SkillList";
-import { Metadata } from "next";
 
-export const metadata = {
-  Metatitle: "Home | Abbas",
-  description: "Software Engineer portfolio ideas",
-  keywords: "portfolio frontend developer",
-  alternates: {
-    canonical: `${process.env.DOMAIN}/home`,
-  },
-};
 export default function HomePage() {
   return (
-    <ContainerAOS>
-      <div className="">
-        <HeaderHome />
-        <Divider orientation="horizontal" className="my-6" />
-        <Career />
-        <Divider orientation="horizontal" className="my-6" />
-        <SkillLists />
-        <Divider orientation="horizontal" className="my-6" />
-        <ContactCard />
-      </div>
-    </ContainerAOS>
+    <>
+      <ContainerAOS>
+        <div className="">
+          <HeaderHome />
+          <Divider orientation="horizontal" className="my-6" />
+          <Career />
+          <Divider orientation="horizontal" className="my-6" />
+          <SkillLists />
+          <Divider orientation="horizontal" className="my-6" />
+          <ContactCard />
+        </div>
+      </ContainerAOS>
+    </>
   );
 }
